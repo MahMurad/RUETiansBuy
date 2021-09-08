536d3b14c28b9ceae899c6d902f50679ed3ea296
Murad <asmaulhossain77@gmail.com>
2021-09-08 11:35:14 +0600

    Before newthings

--- RUETiansBuy/urls.py
+++ RUETiansBuy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('newsfeed/', include('newsfeed.urls')),
+    path('', include('newsfeed.urls')),
 ]
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+ads = [
+    {
+        'seller': 'Student 1',
+        'title': 'Book 1',
+        'category': 'Book',
+        'subject': 'Introduction to web developing',
+        'price': 300,
+        'new': True,
+        'contact_no': '01791234567',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'seller': 'Student 2',
+        'title': 'Book 2',
+        'category': 'Book',
+        'subject': 'Introduction to web developing 2',
+        'price': 350,
+        'new': True,
+        'contact_no': '01791234567',
+        'date_posted': 'August 27, 2018'
+    }
+]
+
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse('<h1>Welcome to RUETiansBuy</h1>')
+    context = {'ads': ads}
+    return render(request, 'newsfeed/home.html', context)
+#, {'title': 'Home'}
+
+
+def about(request):
+    return render(request, 'newsfeed/about.html', {'title': 'About'})
