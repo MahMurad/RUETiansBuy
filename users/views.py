2b92664dbd4d3c08ba4a45a746f291f1cdb94cc9
Murad <asmaulhossain77@gmail.com>
2021-10-07 12:59:05 +0600

    login

--- users/views.py
+++ users/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
+
 def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login(request):
     form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
